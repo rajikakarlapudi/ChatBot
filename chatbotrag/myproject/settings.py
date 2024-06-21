81223df74ebc3e7899062ebc364b58187c642be4
Kakarlapudi Rajeswari <127588101+rajikakarlapudi@users.noreply.github.com>
2024-06-21 22:28:02 +0530

    Update settings.py added api key in envi var

--- chatbotrag/myproject/settings.py
+++ chatbotrag/myproject/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
+API_KEY = os.getenv('API_KEY')
+
+# Ensure you handle cases where the environment variable might not be set
+if not API_KEY:
+    raise ValueError("No API_KEY set for Django application")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
